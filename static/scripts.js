7d4233476a35e8f4ca8de69b38714058e2a5af71
johanneshassenstein <johannes.hassenstein@protonmail.com>
2024-01-25 12:20:51 +0100

    added new videos and better preload logic

--- static/scripts.js
+++ static/scripts.js
@@ -18,25 +18,48 @@ document.addEventListener("DOMContentLoaded",function(){
     document.body.classList.toggle("projectsActive")
     document.body.classList.remove("imprintActive")
     document.body.classList.remove("aboutActive")
+    preloadAll()
     update(s);
   })
   let l = [].slice.call(document.getElementById("projects").getElementsByTagName("li"))
   let s = 0
+  let pre = false
   document.getElementById("scroller").addEventListener("scroll",function(e){
     s = e.target.scrollTop/(e.target.scrollHeight - e.target.clientHeight)
     update(s)
   })
   update(0,true);
   document.body.classList.add("loaded")
+  function preloadAll(){
+    if(!pre){
+      for(let i in l){
+        load(l[i])
+      }
+      pre = true
+    }
+
+  }
+  function load(e){
+    let v = [].slice.call(e.getElementsByTagName("video"))
+    let m = [].slice.call(e.getElementsByTagName("img"))
+    v = v.length ? v[0] : false
+    m = m.length ? m[0] : false
+    if(m && !m.getAttribute("src")){
+      m.src = m.getAttribute("data-src")
+    }
+    if(v && !v.getAttribute("src")){
+      v.src = v.getAttribute("data-src")
+      v.poster = v.getAttribute("data-poster")
+    }
+  }
   function update(p,init){
     let _p = Math.round((l.length - 1) * p)
+    let fV = false
     for(let i in l){
       let _i = i - _p
       let s = Math.pow(1 - (Math.abs(_i) / l.length),2)
       let v = [].slice.call(l[i].getElementsByTagName("video"))
-      let m = [].slice.call(l[i].getElementsByTagName("img"))
       v = v.length ? v[0] : false
-      m = m.length ? m[0] : false
       if(v && !init){
         if( _i == 0){
           v.play()
@@ -45,14 +68,9 @@ document.addEventListener("DOMContentLoaded",function(){
           v.pause()
         }
       }
-      if(init){
-        if(m){
-          m.src = m.getAttribute("data-src")
-        }
-        if(v){
-          v.src = v.getAttribute("data-src")
-          v.poster = v.getAttribute("data-poster")
-        }
+      if(init && !fV && v){
+        load(l[i])
+        fV = true
       }
       l[i].style.transform = "translate(" + ((_i * 50)*s - 50) + "%,"  + ((_i * 50)*s - 50)  + "%) scale(" + 100 * s + "%)"
       l[i].style.zIndex = _i < 0 ? 100 + _i : 100 - _i
