e278d6b907b6b43240539068815140c137a307c4
johanneshassenstein <johannes.hassenstein@protonmail.com>
2024-01-25 10:27:27 +0100

    added font, moved script, improved initial load

--- static/scripts.js
+++ static/scripts.js
@@ -18,26 +18,40 @@ document.addEventListener("DOMContentLoaded",function(){
     document.body.classList.toggle("projectsActive")
     document.body.classList.remove("imprintActive")
     document.body.classList.remove("aboutActive")
+    update(s);
   })
   let l = [].slice.call(document.getElementById("projects").getElementsByTagName("li"))
+  let s = 0
   document.getElementById("scroller").addEventListener("scroll",function(e){
-    let p = e.target.scrollTop/e.target.scrollTopMax
-    update(p)
+    s = e.target.scrollTop/e.target.scrollTopMax
+    update(s)
   })
-  update(0,false);
-  function update(p,_v){
+  update(0,true);
+  document.body.classList.add("loaded")
+  function update(p,init){
     let _p = Math.round((l.length - 1) * p)
     for(let i in l){
       let _i = i - _p
       let s = Math.pow(1 - (Math.abs(_i) / l.length),2)
       let v = [].slice.call(l[i].getElementsByTagName("video"))
+      let m = [].slice.call(l[i].getElementsByTagName("img"))
       v = v.length ? v[0] : false
-      if(v && !_v){
+      m = m.length ? m[0] : false
+      if(v && !init){
         if( _i == 0){
           v.play()
         }
         else{
           v.pause()
+        }
+      }
+      if(init){
+        if(m){
+          m.src = m.getAttribute("data-src")
+        }
+        if(v){
+          v.src = v.getAttribute("data-src")
+          v.poster = v.getAttribute("data-poster")
         }
       }
       l[i].style.transform = "translate(" + ((_i * 50)*s - 50) + "%,"  + ((_i * 50)*s - 50)  + "%) scale(" + 100 * s + "%)"
